2c84f917ca271af7c97c44ab3e4906060c5b7a5a
Bill Cordias <billcordias8@gmail.com>
2022-10-31 00:13:43 +0300

    update

---
+++ Object_Or.Prog/Simple grading system/grading.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<stdlib.h>
+#include<student.h>
+using namespace std;
+
+class student
+{
+    private:
+    string name;
+    string regnum;
+    double sub1, sub2, sub3;
+
+    public:
+    void setvalues(string, string, double, double, double);
+    double calc-mean();
+    char get-grade();
+};
