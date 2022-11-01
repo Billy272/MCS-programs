fc5d067721637121c200fce8a288a120ef3ed517
Bill Bitok <98650586+Billy272@users.noreply.github.com>
2022-11-01 19:00:37 +0300

    Add files via upload

---
+++ Object_Or.Prog/bank.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+class customer;
+{
+    private:
+    string customer_name();
+    int account_num();
+    double balance;
+    public:
+    void read_data();
+    void deposit();
+    void witdraw();
+    double checkbalance();
+    void display();
+}
+//implementation
+void customer::read_data();
+{
+    cout<<"Enter name: "<<endl;
+    cin>>customer_name;
+    cout<<"Enter Account number: "<<endl;
+    cin>>account_num;
+    cout<<"Enter balance: "<<endl;
+    cin>>balance;
+}
+void customer::deposit()
+{
+    double amount;
+    cout<<"Amount to deposit: ";
+    cin>>amount;
+    balance+=amount;
+    cout<<"New balance: "<<balance;
+}
+
+void customer::withdraw()
+{
+    double amount;
+    cout<<"Amount to widthraw: ";
+    cin>>amount;
+    balance-=amount;
+    cout<<"New balance: "<<balance;
+}
+
+double customer::checkbalance()
+{
+    return balance;
+}
+void customer::display()
+{
+    cout<<"Name: "<<customer_name;
+    cout<<"Account Number: "<<account_num;
+    cout<<"Account balance: "<<balance;
+}
+int main()
+{
+    customer c;
+    int x;
+    c.read_data();
+    c.display();
+    cout<<"Press 1 to Deposit"<<endl;
+    cout<<"Press 2 to Widthraw"<<endl;
+    cout<<"Press 3 to Check balance"<<endl;
+    cin>>x;
+
+    if(x==1)
+        c.deposit();
+    else if(x==2)
+        c.widthdraw();
+    else(x==3)
+        c.checkbalance();
+    return 0;
+
+}
